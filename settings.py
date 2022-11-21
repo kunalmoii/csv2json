43382388ded2ef7c73ea64f24e9de35eba0f6f43
Kunal <kunal@moiiai.com>
2022-11-21 11:43:11 +0530

    minor changes

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 from easydict import EasyDict as edict
 
 __APP_SETTINGS__ = edict()
-__APP_SETTINGS__.CSV_PATH = 'poter.csv'
+__APP_SETTINGS__.CSV_PATH = 'poter2.csv'
 __APP_SETTINGS__.CLASS_NAMES = ['Person', 'Package', 'Dog']
-__APP_SETTINGS__.OUTPUT_JSON = 'data.json'
+__APP_SETTINGS__.OUTPUT_JSON = 'data2.json'
